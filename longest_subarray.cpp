b30fab63713b7b8f15a54e75845f567275a3537a
Aditya Nigam <75134530+Adi2209@users.noreply.github.com>
2022-02-27 22:02:11 +0530

    Add files via upload

---
+++ longest_subarray.cpp
@@ -0,0 +1,31 @@
+//longest arithmetic subarray google kickstart xD
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    int a[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+
+    int ans;
+    int i ;
+    int pd = a[i] - a[i + 1];
+    int j;
+    for (int j = i; j < n; j++)
+    {
+        if (pd=a[j]-a[j+1])
+        {
+            /* code */
+        }
+        
+    }
+    
+
+    return 0;
+}
