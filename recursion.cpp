6f0c9521c80b0b6ba1ea88c30a032fe14a8f4598
Aditya Nigam <75134530+Adi2209@users.noreply.github.com>
2022-03-04 11:42:34 +0530

    Add files via upload

---
+++ recursion.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+     int x=10;
+     int *px=&x,**ppx=&px;
+     **ppx=x+(**ppx)+(-(*px))+x;
+     printf("%d",x);
+
+    return 0;
+}
