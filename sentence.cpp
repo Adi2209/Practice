f291788919feb644dcdbed77ffddff3f0886ed0c
Aditya Nigam <75134530+Adi2209@users.noreply.github.com>
2022-03-06 13:36:30 +0530

    Add files via upload

---
+++ sentence.cpp
@@ -0,0 +1,22 @@
+// find the largest word in a sentence
+
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    cin.ignore();
+    char arr[n + 1];
+    cin.getline(arr,n);
+    cin.ignore();
+
+    for (int i = 0; i < n; i++)
+    {
+        dec
+    }
+
+    return 0;
+}
