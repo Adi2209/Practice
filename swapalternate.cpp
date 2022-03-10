4ca63e757ebabea68b5222e9cc4f7f2953274354
Aditya Nigam <75134530+Adi2209@users.noreply.github.com>
2022-03-10 22:42:14 +0530

    Add files via upload

---
+++ swapalternate.cpp
@@ -0,0 +1,32 @@
+//wacp to swap alternate int
+
+#include<iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+  int n;
+  cout<<"Enter the size of array  : "<<endl;
+  int arr[n];
+  cout<<"Enter the elements of the array : "<<endl;
+  for (int i = 0; i < n; i++)
+  {
+      cin>>arr[i];
+  }
+  int l=0, r=n-1;
+  while(l<r)
+
+  for (int i = 0; i < n; i++)
+  {
+      cout<<arr[i]<<" ";
+  }
+
+
+
+
+
+
+
+  
+    return 0;
+}
