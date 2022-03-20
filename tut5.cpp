c65953a2e4d647d9f7a7452e0ed2ac4a125a849d
Aditya Nigam <75134530+Adi2209@users.noreply.github.com>
2022-03-20 13:13:19 +0530

    Add files via upload

---
+++ tut5.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int num1, num2;
+    cout<<"enter the value of num1:\n "; // this is called insertion operator
+    cin>>num1; // is called extraction opertaor
+    cout<<"enter the value of num2:\n ";
+    cin>>num2; // is called extraction opertaor
+    
+    cout<<"The sum is" <<num1+num2;
+
+return 0;
+}
